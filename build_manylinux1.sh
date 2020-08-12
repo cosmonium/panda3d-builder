02be2e58bb8965581f5c9dd426c31f5f249df36f
LD <laurent.deru@gmail.com>
2020-08-12 21:50:52 +0200

    Finalize scripts for manylinux1_X86_64

--- build_manylinux1.sh
+++ build_manylinux1.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+set -x
+
 #TODO: extract version from git
 VERSION="1.10.6.fp64+opt"
 
 ABI=cp37-cp37m
 PLATFORM=x86_64
-WHEEL=%s-$ABI-$PLATFORM.whl
 
-docker run -t --rm=true -v `pwd`/build/:/build/:rw -w /build/ cosmoniumeldee/panda3d-manylinux1 /opt/python/$ABI/bin/python makepanda/makepanda.py \
+yum install -y rpm-build fakeroot zip
+
+ln -s /opt/python/$ABI/bin/python /usr/bin/python3
+
+/opt/python/$ABI/bin/python makepanda/makepanda.py \
 --everything \
 --no-gles --no-gles2 --no-egl \
 --python-incdir=/opt/python/$ABI/include --python-libdir=/opt/python/$ABI/lib \
@@ -15,8 +20,13 @@ docker run -t --rm=true -v `pwd`/build/:/build/:rw -w /build/ cosmoniumeldee/pan
 --nocolor \
 --use-sse2 \
 --distributor=cosmonium \
---git-commit=`cd build && git rev-parse HEAD` \
+--git-commit=`git rev-parse HEAD` \
 --threads=4 \
 --optimize=4 \
 --override STDFLOAT_DOUBLE=1 \
---wheel --version $VERSION
+--wheel --version $VERSION \
+--installer
+
+cd /root/thirdparty/linux-libs-x64/eigen/include
+zip -r extra-includes.zip Eigen/
+mv extra-includes.zip /build
