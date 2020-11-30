c7ae3c8d9e6d50833342d6378ddcc58b13dd5f65
LD <laurent.deru@gmail.com>
2020-11-30 17:54:34 +0100

    Fix build on macOS

--- build_macos.sh
+++ build_macos.sh
@@ -2,15 +2,17 @@
 
 set -x
 
+PYTHON_VERSION=3.7
+
 #TODO: extract version from git
 COUNT=`git rev-list --count v1.10.7..HEAD`
 VERSION="1.10.7.dev${COUNT}+fp64+opt"
 
-PYTHON=/usr/local/opt/python@3.7/bin/python3
+PYTHON=/usr/local/opt/python\@$PYTHON_VERSION/bin/python3
 
 $PYTHON makepanda/makepanda.py \
---python-incdir /usr/local/Cellar/python/3.7.5/Frameworks/Python.framework/Versions/3.7/include \
---python-libdir /usr/local/Cellar/python/3.7.5/Frameworks/Python.framework/Versions/3.7/lib \
+--python-incdir /usr/local/opt/python\@$PYTHON_VERSION/Frameworks/Python.framework/Versions/$PYTHON_VERSION/include \
+--python-libdir /usr/local/opt/python\@$PYTHON_VERSION/Frameworks/Python.framework/Versions/$PYTHON_VERSION/lib \
 --osxtarget=10.9 \
 --everything \
 --no-gles --no-gles2 --no-egl \
--- init_macos.sh
+++ init_macos.sh
@@ -3,3 +3,5 @@
 # Download XCode 6.4 from https://developer.apple.com/devcenter/download.action?path=/Developer_Tools/Xcode_6.4/Xcode_6.4.dmg
 
 #Copy MacOSX10.9.sdk to /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs
+
+# sudo cp -R /Volumes/Xcode/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs
